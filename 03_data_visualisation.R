5b7a363f6b0ff7fdd19750b6ff141d07ab6e0744
Matt Wilson <matt.wilson@ukhsa.gov.uk>
2024-09-17 12:17:06 +0100

    Added code for loading and exploring gapminder data.

---
+++ 03_data_visualisation.R
@@ -0,0 +1,12 @@
+# Data visualisation session
+
+library(ggplot2)
+gapminder <- read.csv("data/gapminder_data.csv")
+
+gapminder
+View(gapminder)
+str(gapminder)
+head(gapminder)
+typeof(gapminder)
+typeof(gapminder$country)
+
