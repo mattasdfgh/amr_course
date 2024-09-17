fe853b625efc3be80c70a68552fc90835e9f8ea4
Matt Wilson <matt.wilson@ukhsa.gov.uk>
2024-09-17 12:27:08 +0100

    Added some working ggplot code

--- 03_data_visualisation.R
+++ 03_data_visualisation.R
@@ -10,3 +10,14 @@ head(gapminder)
 typeof(gapminder)
 typeof(gapminder$country)
 
+#Using ggplot2
+# We will start by doing everything the slow way.
+#1. Relationship between GDP per capita and life expectancy
+ggplot(data = gapminder, mapping = aes(x = gdpPercap, y = lifeExp)) +
+         geom_point()
+
+
+
+
+
+
