a967ce55d6ec6c437eb956322d77a8af491d7ef7
Matt Wilson <matt.wilson@ukhsa.gov.uk>
2024-09-17 12:33:42 +0100

    Added some bad code, needs fixing

--- 03_data_visualisation.R
+++ 03_data_visualisation.R
@@ -10,14 +10,23 @@ head(gapminder)
 typeof(gapminder)
 typeof(gapminder$country)
 
-#Using ggplot2
+# Using ggplot2
 # We will start by doing everything the slow way.
 #1. Relationship between GDP per capita and life expectancy
 ggplot(data = gapminder, mapping = aes(x = gdpPercap, y = lifeExp)) +
-         geom_point()
+  geom_point()
 
+#2 How does life expectancy change over time
+ggplot(gapminder, aes(year, lifeExp)) +
+  geom_point()
 
+# This gives us points for all countries, which looks bad, here is an example for just one country
+ggplot(gapminder[gapminder$country=="United Kingdom", ], aes(year, lifeExp)) +
+  geom_point()
 
+# There are some other things we can do here, some of which give terrible results
+ggplot(gapminder, aes(year, lifeExp)) +
+  geom_line()
 
 
 
