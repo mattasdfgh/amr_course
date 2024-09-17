3c18ca53fd95ba5468d838e0424dbae1b0ad5fc9
Matt Wilson <matt.wilson@ukhsa.gov.uk>
2024-09-17 13:01:07 +0100

    Added some more plots to demonstrate new functionalities.

--- 03_data_visualisation.R
+++ 03_data_visualisation.R
@@ -44,10 +44,35 @@ ggplot(gapminder, aes(year, lifeExp)) +
 
 # Note that changing the order of the layers makes a difference. 
 
+#----------------
+
+# Now let's check if we have Vietnam in the data
+"Vietnam" %in% gapminder$country
+gap_vietnam <- gapminder[gapminder$country == "Vietnam", ]
+
+ggplot(gap_vietnam, aes(year, lifeExp)) +
+  geom_point() +
+  geom_line(aes(color = continent, group = country))
 
 
-  
+# Let's add a second country
+gap_viet_uk <- gapminder[gapminder$country == "Vietnam" | gapminder$country == "United Kingdom", ]
 
+ggplot(gap_viet_uk, aes(year, lifeExp)) +
+  geom_point() +
+  geom_line(aes(color = continent, group = country))
+
+# Play with some other arguments
+ggplot(gapminder, aes(gdpPercap, lifeExp)) +
+  geom_point(alpha = 0.1, mapping = aes(color = continent))
+
+# Transformations (the bottom one is better because it automatically fixes the x axis labels)
+ggplot(gapminder, aes(log10(gdpPercap), lifeExp)) +
+  geom_point(alpha = 0.1, mapping = aes(color = continent))
+
+ggplot(gapminder, aes(gdpPercap, lifeExp)) +
+  geom_point(alpha = 0.1, mapping = aes(color = continent)) +
+  scale_x_log10()
 
 
 
