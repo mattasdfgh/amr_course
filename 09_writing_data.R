0865577077a443f5f55cd2f1a9cf857219b7799a
Matt Wilson <matt.wilson@ukhsa.gov.uk>
2024-09-19 11:08:25 +0100

    Lots of changes

--- 09_writing_data.R
+++ 09_writing_data.R
@@ -1,6 +1,8 @@
 # Writing data
 
 library(ggplot2)
+library(tidyr)
+library(dplyr)
 gapminder <- read.csv("data/gapminder_data.csv")
 
 # Some plots
@@ -17,8 +19,17 @@ ggsave("results/life_expectancy_by_continent.pdf", cont_plot, width = 6, height 
 
 
 ## Exporting data (write.csv exports row numbers as column 1 as default)
-gap_australia <- gapminder[gapminder$country == "Australia", ]
+gap_australia <- gapminder |> 
+  filter(country == "Australia")
 write.csv(gap_australia, "processing/full_australia.csv")
 
+import_aus <- read.csv("processing/full_australia.csv")
+import_aus
+gap_australia
 
+# We can export basically any object from what I can tell. Obviously very useful for data frames.
+x <- 2
+write.csv(x, "processing/x.csv")
+y <- "as124"
+write.csv(y, "processing/y.csv")
 
